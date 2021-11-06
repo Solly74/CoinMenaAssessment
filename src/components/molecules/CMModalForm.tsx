bcf70c5f05042af2eceb8e7f8a1865b9433127da
solly <solly74@gmail.com>
2021-11-06 21:07:37 +0200

    close button for modal form

--- src/components/molecules/CMModalForm.tsx
+++ src/components/molecules/CMModalForm.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View, StyleSheet } from "react-native";
 import { CMModal } from "../atoms";
 import { IModal } from "../../constants/Types";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { THEME } from "../../constants";
 
 const CMModalForm: React.FC<IModal> = ({
   visible = false,
@@ -10,16 +12,26 @@ const CMModalForm: React.FC<IModal> = ({
 }) => {
   return (
     <CMModal visible={visible} dismiss={dismiss}>
-      <View
-        style={{
-          flex: 0.8,
-          width: "90%",
-        }}
-      >
+      <View style={styles.parent}>
+        <TouchableOpacity
+          onPress={dismiss}
+          hitSlop={{ left: 5, bottom: 5 }}
+          style={styles.closeButton}
+        >
+          <MaterialIcons name={"close"} size={25} color={THEME.Colors.white} />
+        </TouchableOpacity>
         {children}
       </View>
     </CMModal>
   );
 };
+
+const styles = StyleSheet.create({
+  parent: {
+    flex: 0.8,
+    width: "90%",
+  },
+  closeButton: { right: 5, top: 30, zIndex: 1, alignSelf: "flex-end" },
+});
 
 export default CMModalForm;
